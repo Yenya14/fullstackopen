9aaedc2d15c9d97cdcdd33ed226c62312a7e9a39
Yenya14 <leeza.mhr@gmail.com>
2023-07-19 10:04:05 +0545

    remaining commit

--- part1/courseinfo/src/Total.js
+++ part1/courseinfo/src/Total.js
@@ -1,4 +1,9 @@
-const Total = (props) => (
-    <p>Number of exercises {props.total}</p>
-)
-export default Total;
+const Total = (props) => {
+    const exercisesArray = props.parts.map((part) => part.exercises); 
+    const totalExercises = exercisesArray.reduce((total, exercises) => total + exercises, 0);
+    return (
+      <p>Number of exercises {totalExercises}</p>
+    );
+  };
+  
+  export default Total;
