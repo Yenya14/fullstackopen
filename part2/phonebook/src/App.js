4c0866049212f69b7c439597d2f849167dd67411
Yenya14 <leeza.mhr@gmail.com>
2023-08-22 22:06:01 +0545

    css phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,6 +2,7 @@ import { useState, useEffect} from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import Notification from './components/Notification'
 import servicePerson from './services/servicePerson';
 
 const App = () => {
@@ -9,6 +10,8 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterQuery, setFilterQuery] = useState('')
+  const [status, setStatus] = useState(null)
+  const [poppedMessage, setPoppedMessage] = useState(null)
 
 useEffect(() => {
   servicePerson.getAll().then((result) => setPersons(result))
@@ -28,19 +31,37 @@ useEffect(() => {
     if (condition){
       if (window.confirm(`${newName} is already added to phonebook, replace a old number with the new one?`)){
         servicePerson.update(condition.id, newPerson).then((result) => {
-          setPersons(persons.map(person => (person.id !== condition.id ? person : result)))
+          setPersons(persons.map((person) => (person.id !== condition.id ? person : result)))
+        })
+        .catch((err)=> {
+          setStatus('error')
+          setPoppedMessage(`Information of ${condition.name} has already been removed from the server`)
+          setTimeout(() => {
+            setStatus(null)
+            setPoppedMessage(null);
+          }, 5000);
+
+          setPersons(persons.filter((person) => person.id !== condition.id))
         })
       }
     }
-    else{
-  servicePerson.create(newPerson)
-  .then(personAdded => 
-    setPersons(persons.concat(personAdded)))
-  
-  setNewName("")
-  setNewNumber('')
-}}
+    else {
+      servicePerson.create(newPerson)
+        .then(personAdded => {
+          setPersons(persons.concat(personAdded));
 
+          setStatus('popped')
+          setPoppedMessage(`Added ${personAdded.name}`);
+          setTimeout(() => {
+            setStatus(null)
+            setPoppedMessage(null);
+          }, 5000);
+        });
+    }
+    
+    setNewName("");
+    setNewNumber("");
+  }
 const deletePerson =(id, name) => () => {
   if(window.confirm(`Delete ${name}?`)){
     servicePerson.remove(id).then(() =>{
@@ -53,6 +74,7 @@ const deletePerson =(id, name) => () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={poppedMessage} status={status}/>
       <Filter query={filterQuery} handleChangedName={handleChangedName(setFilterQuery)}/>
       <h3>add a new</h3>
       <PersonForm handleChangedName={handleChangedName(setNewName)} 
